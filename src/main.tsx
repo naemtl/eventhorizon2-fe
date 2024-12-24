5a392e7a9c396bdb1248bfeaff6cd499aef6928c
Matthew Stinis <matthew.stinis@onespan.com>
2024-12-24 01:29:38 -0500

    add routing + base component structure + dummy data + linting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,37 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+import Calendar from "./pages/Calendar/Calendar";
+import EventListing from "./pages/EventListing/EventListing";
+import FAQ from "./pages/FAQ/FAQ";
+import NotFound from "./pages/NotFound/NotFound";
+import Results from "./pages/Results/Results";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Calendar />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/event/:originalId",
+    element: <EventListing />,
+  },
+  {
+    path: "/faq",
+    element: <FAQ />,
+  },
+  {
+    path: "/results",
+    element: <Results />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
--- src/pages/FAQ/FAQ.tsx
+++ src/pages/FAQ/FAQ.tsx
@@ -1,7 +1,5 @@
 function FAQ() {
-  return (
-    <div>FAQ</div>
-  )
+  return <div>FAQ</div>;
 }
 
-export default FAQ
+export default FAQ;
