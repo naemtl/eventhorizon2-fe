397b884490caa45f3b7a4d4cfb9dc0ff9cb665c8
Matthew Stinis <mpstinis@gmail.com>
2024-12-24 21:58:14 -0500

    start eventcard + calendar development

--- src/components/EventCard/EventCard.tsx
+++ src/components/EventCard/EventCard.tsx
@@ -1,7 +1,18 @@
-function EventCard() {
+import { EventCardProps } from "./EventCard.types";
+
+function EventCard({ event }: EventCardProps) {
   return (
-    <div>EventCard</div>
-  )
+    <div>
+      <img src={event.image ?? ""} alt={event.title} />
+      <h2>{event.title}</h2>
+      <p>{event.dateShowTime}</p>
+      <p>{event.venue}</p>
+      <p>{event.address}</p>
+      <p>{event.price}</p>
+      <p>{event.moreInfoLink}</p>
+      <p>{event.source}</p>
+    </div>
+  );
 }
 
-export default EventCard
+export default EventCard;
--- src/pages/EventListing/EventListing.tsx
+++ src/pages/EventListing/EventListing.tsx
@@ -2,8 +2,32 @@ import { useLocation } from "react-router-dom";
 
 function EventListing() {
   const { state } = useLocation();
+  console.log(state);
 
-  return <div>EventListing</div>;
+  const {
+    title,
+    dateShowTime,
+    preciseTime,
+    venue,
+    address,
+    price,
+    image,
+    moreInfoLink,
+    source,
+  } = state.event;
+
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <p>{dateShowTime}</p>
+      <p>{venue}</p>
+      <p>{address}</p>
+      <p>{price}</p>
+      <p>{moreInfoLink}</p>
+      <p>{source}</p>
+    </div>
+  );
 }
 
 export default EventListing;
