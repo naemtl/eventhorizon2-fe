eb57d0c8d3adedffb32e2b2490a4c8e5222b9945
Matthew Stinis <matthew.stinis@onespan.com>
2024-12-30 01:21:25 -0500

    add styles for eventlisting + addtocalendar button

--- src/pages/EventListing/EventListing.tsx
+++ src/pages/EventListing/EventListing.tsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 
 import styles from "./EventListing.module.css";
 import dayjs from "dayjs";
+import { AddToCalendarButton } from "add-to-calendar-button-react";
 
 function EventListing() {
   const { state } = useLocation();
@@ -20,27 +21,45 @@ function EventListing() {
   } = state.event;
 
   const parsedDate = useMemo(
-    () => dayjs(dateShowTime).format("DD.MM.YYYY HH:mm"),
+    () => dayjs(dateShowTime).format("DD.MM.YYYY hh:mm"),
     [dateShowTime]
   );
 
+  const addToCalendarDateTime = useMemo(
+    () => dayjs(dateShowTime).format("YYYY-MM-DD hh:mm"),
+    [dateShowTime]
+  );
+
+  const addToCalendarTimeArray = useMemo(
+    () => addToCalendarDateTime.split(" "),
+    [addToCalendarDateTime]
+  );
+
+  console.log(addToCalendarTimeArray);
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.imgContainer}>
-          <img src={image} alt={title} />
-        </div>
-        <h1>{title}</h1>
+      <div className={styles.imgContainer}>
+        <img src={image} alt={title} />
       </div>
       <div className={styles.infoContainer}>
+        <h1>{title}</h1>
+        <div>Date: {parsedDate}</div>
         <div>
-          <div>{parsedDate}</div>
-          <div>
-            {venue} - {address}
-          </div>
-          <div>{price ?? "Check source for cost of entry"}</div>
-          <a href={moreInfoLink ?? ""}>More info - {source}</a>
+          Location: {venue} - {address}
         </div>
+        <div>Price: {price ?? "Check source for cost of entry"}</div>
+        <a href={moreInfoLink ?? ""}>More info: {source}</a>
+        <AddToCalendarButton
+          name={title}
+          startDate={addToCalendarTimeArray[0]}
+          // startTime={addToCalendarTimeArray[1]} FIXME: not working
+          endDate={addToCalendarTimeArray[0]}
+          // endTime={addToCalendarTimeArray[1]}
+          location={`${venue} - ${address}`}
+          options={["Google", "iCal", "Outlook.com"]}
+          lightMode="dark"
+        />
       </div>
     </div>
   );
