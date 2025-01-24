a93bc651f1c04a237ae8e5673339c8579ea43b7f
Matthew Stinis <mpstinis@gmail.com>
2025-01-24 01:30:20 -0500

    finish About faq accordion

--- src/components/Accordion/Accordion.tsx
+++
@@ -1,5 +0,0 @@
-function Accordion() {
-  return <div></div>;
-}
-
-export default Accordion;
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,24 +1,29 @@
 import { useTranslation } from "react-i18next";
+import { Accordion, AccordionItem } from "@szhsin/react-accordion";
+
+import styles from "./About.module.css";
+
+import "./Accordion.css";
 
 function About() {
   const { t } = useTranslation();
 
-  const questionsAndAnswers = [
-    {
-      question: t("faq.question"),
-      answer: t("faq.answer"),
-    },
-    {
-      question: t("faq.question"),
-      answer: t("faq.answer"),
-    },
-    {
-      question: t("faq.question"),
-      answer: t("faq.answer"),
-    },
-  ];
-
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>{t("about.title")}</h1>
+      <Accordion>
+        <AccordionItem header={t("faq.question")}>
+          {t("faq.answer")}
+        </AccordionItem>
+        <AccordionItem header={t("faq.question")}>
+          {t("faq.answer")}
+        </AccordionItem>
+        <AccordionItem header={t("faq.question")}>
+          {t("faq.answer")}
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
 }
 
 export default About;
