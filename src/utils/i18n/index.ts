2559b12a050ad5fb36b7fc2014a7eb289f28cfe8
Matthew Stinis <mpstinis@gmail.com>
2025-02-02 23:23:14 -0500

    remove lng property from i18n index

--- src/utils/i18n/index.ts
+++ src/utils/i18n/index.ts
@@ -25,10 +25,6 @@ i18n
     resources,
     supportedLngs: ['en', 'es', 'fr'],
     fallbackLng: "en",
-    lng: "en", // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
     interpolation: {
       escapeValue: false // react already safes from xss
     }
