fa3c54370a4764454a65fb8ccc74e5b515c9f7df
Matthew Stinis <mpstinis@gmail.com>
2025-04-11 20:23:36 -0400

    add EventListing page on eventId link

--- src/pages/EventListing/EventListing.tsx
+++ src/pages/EventListing/EventListing.tsx
@@ -1,18 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
 import { getRouteApi } from '@tanstack/react-router';
 import { memo } from 'react';
 
-import { fetchEventListing } from 'src/api/eventListing.ts';
 import EventListingDetails from 'src/components/EventListingDetails/EventListingDetails.tsx';
 
-function EventListing() { // TODO: Pass event id
+function EventListing() {
   const route = getRouteApi('/events/$eventId');
-  const { event } = route.useLoaderData();
-
-  // const { data: event } = useQuery({
-  //   queryKey: ['events', { eventId }],
-  //   queryFn: () => fetchEventListing(eventId),
-  // });
+  const event = route.useLoaderData();
 
   return (
     <div>
