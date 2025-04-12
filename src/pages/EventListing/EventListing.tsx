69d64d8863c44f3c6fabae0359a3f14a73abe03c
Matthew Stinis <mpstinis@gmail.com>
2025-04-12 19:08:57 -0400

    change eventlisting styles

--- src/pages/EventListing/EventListing.tsx
+++ src/pages/EventListing/EventListing.tsx
@@ -3,12 +3,14 @@ import { memo } from 'react';
 
 import EventListingDetails from 'src/components/EventListingDetails/EventListingDetails.tsx';
 
+import styles from './EventListing.module.css';
+
 function EventListing() {
   const route = getRouteApi('/events/$eventId');
   const event = route.useLoaderData();
 
   return (
-    <div>
+    <div className={styles.container}>
       <EventListingDetails event={event} />
     </div>
   );
